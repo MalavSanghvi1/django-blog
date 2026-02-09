8b97ea1b73d56bbe5203d949329b03546da18409
Malav Sanghvi <malavsanghvi2005@gmail.com>
2026-02-09 23:58:30 +0530

    Fix profile image handling for Render (static fallback + safe resize)

--- users/models.py
+++ users/models.py
@@ -2,20 +2,38 @@ from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
 
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-   # image = models.ImageField(default='default.jpg', upload_to='profile_pics')
-    image = models.ImageField(upload_to='profile_pics', blank=True, null=True)
 
+    # IMPORTANT: media folder ignored on Render, so do NOT force a default media file here.
+    image = models.ImageField(upload_to="profile_pics", blank=True, null=True)
 
     def __str__(self):
-        return f'{self.user.username} Profile'
+        return f"{self.user.username} Profile"
 
     def save(self, *args, **kwargs):
+        # Save first so file path exists (if uploaded)
         super().save(*args, **kwargs)
 
-        # (optional) only if you're resizing like Corey
-        img = Image.open(self.image.path)
-        if img.height > 300 or img.width > 300:
-            img.thumbnail((300, 300))
-            img.save(self.image.path)
+        # If no image uploaded, do nothing (avoid crash on Render)
+        if not self.image:
+            return
+
+        # If storage doesn't provide local path (some deployments), safely skip resize
+        try:
+            img_path = self.image.path
+        except Exception:
+            return
+
+        try:
+            img = Image.open(img_path)
+
+            # Resize only if bigger than 300x300
+            if img.height > 300 or img.width > 300:
+                output_size = (300, 300)
+                img.thumbnail(output_size)
+                img.save(img_path)
+        except Exception:
+            # If Pillow can't open/resize for any reason, don't crash production
+            return
